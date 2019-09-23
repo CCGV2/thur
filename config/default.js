8b5055a4dff9adddb9ace32ecc527c20eee77fe6
耿琮凯 <gengcongkai789123@163.com>
2019-09-23 17:32:13 +0800

    success on windows

--- config/default.js
+++ config/default.js
@@ -5,6 +5,6 @@ module.exports = {
     key: 'thur',
     maxAge: 2592000000
   },
-  mongodb:`mongodb://gck:gengcongkai@cluster0-shard-00-00-yqbl4.azure.mongodb.net:27017,cluster0-shard-00-02-yqbl4.azure.mongodb.net:27017,cluster0-shard-00-01-yqbl4.azure.mongodb.net:27017/thur?ssl=true&replicaSet=Pydow-shard-0&authSource=admin&retryWrites=true`
+  mongodb:"mongodb://gck:gengcongkai@cluster0-shard-00-00-yqbl4.azure.mongodb.net:27017,cluster0-shard-00-02-yqbl4.azure.mongodb.net:27017,cluster0-shard-00-01-yqbl4.azure.mongodb.net:27017/"
 
 };
