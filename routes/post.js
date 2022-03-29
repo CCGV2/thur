ef7606ba3b5d0c81aa29ae5aa79e1cba1b5cc24f
Congkai Geng <gengcongkai789123@163.com>
2022-03-29 10:56:20 -0400

    post1

--- routes/post.js
+++ routes/post.js
@@ -5,7 +5,25 @@ var checkLogin = require('../middlewares/check').checkLogin;
 
 const post_controller = require('../controller/postController');
 
-router.get('/:postID', post_controller.index);
+router.get('/:postID', (req, res) => {
+	// check whether this diagram is belong to this user.
+	var id = req.params.postID;
+	console.log("post index");
+	console.log(id);
+	Diagram.findOne({_id:id}).exec(function(err, diagram) {
+		if (err) {
+			console.log('未找到该模型');
+			return res.redirect('back');
+		}
+		if (req.session.user._id != diagram.author) {
+			console.log('模型与作者不匹配');
+			return res.redirect('back');
+		}
+		req.session.content = diagram.content;
+		req.session.page = diagram.type;
+		return res.render(diagram.type, {content:diagram.content, diagramTitle:diagram.title});
+	})
+});
 
 router.post('/:postID/save', post_controller.save);
 
